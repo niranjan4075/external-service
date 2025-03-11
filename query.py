37d07a1b4b87fe502b27cb42b398b13815808e81
niranjan4075 <niranjanb4075@gmail.com>
2025-03-11 11:46:52 -0500

    Create query.py

---
+++ query.py
@@ -0,0 +1,26 @@
+results = (
+        db.query(
+            Request.first_name,
+            Request.last_name,
+            Request.recipient_email,
+            Request.requester_email,
+            Request.phone_number,
+            Device.device_name,
+            Device.device_model,
+            Device.device_type,
+            Device.device_id
+        )
+        .join(
+            Device,
+            cast(
+                func.regexp_replace(
+                    func.split_part(Request.device_quantities, ':', 1),
+                    '[^0-9]',
+                    '',
+                    'g'
+                ), 
+                Integer
+            ) == Device.device_id
+        )
+        .all()
+    )
