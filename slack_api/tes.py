70122814336d64159500dbb6d98d56ae33daac1f
niranjan4075 <niranjanb4075@gmail.com>
2025-07-13 00:32:37 -0500

    Update tes.py

--- slack_api/tes.py
+++ slack_api/tes.py
@@ -1,3 +1,8 @@
-src/v1/encrypt_request_pb2.py
-src/v1/encrypt_response_pb2.py
-src/v1/cryptor_service_pb2_grpc.py
+python -m grpc_tools.protoc \
+  -Isrc \
+  --python_out=src \
+  --grpc_python_out=src \
+  src/v1/encrypt_request.proto \
+  src/v1/encrypt_response.proto \
+  src/v1/cryptor_service.proto
+
